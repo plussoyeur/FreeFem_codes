b7723c2dd5a64fd83412ea1787d973a9c53b30d8
plussoyeur <emeriau.pe@gmail.com>
2017-03-22 15:11:32 +0100

    Petites modifications. fplot et splot pour savoir si on veut affiche les plot dans stats_vortex.edp

--- py_stats.py
+++ py_stats.py
@@ -1,8 +1,8 @@
-from numpy import *
+import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 
-data = loadtxt("stats.dat")
+data = np.loadtxt("stats.dat",skiprows=1)
 
 def column(matrix,i):
     return [row[i] for row in matrix]
@@ -13,9 +13,9 @@ Ecc = column(data,2)
 sharpness = column(data,3)
 
 
-bin_means, bin_edges, binnumber = stats.binned_statistic(x,sharpness,statistic='mean',bins=[0,2,4,6,8,10,12])
+bin_means, bin_edges, binnumber = stats.binned_statistic(x,Ecc,statistic='mean',bins=[0,2,4,6,8,10,12])
 
-plt.plot(x,sharpness,'b.')
+plt.plot(x,Ecc,'b.')
 plt.hlines(bin_means,bin_edges[:-1],bin_edges[1:],colors='g',lw=5)
 plt.title("Ampl vs distance from center")
 plt.show()
