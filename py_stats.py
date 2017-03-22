836325fae6db798ac356c356edd3de295e79b81d
plussoyeur <emeriau.pe@gmail.com>
2017-03-22 17:08:10 +0100

    Mod python script

--- py_stats.py
+++ py_stats.py
@@ -12,10 +12,37 @@ R = column(data,1)
 Ecc = column(data,2)
 sharpness = column(data,3)
 
+xmax = max(x)
+N = 10
 
-bin_means, bin_edges, binnumber = stats.binned_statistic(x,Ecc,statistic='mean',bins=[0,2,4,6,8,10,12])
+bin_meansR, bin_edgesR, binnumberR = stats.binned_statistic(x,R,statistic='mean',bins=10)
 
-plt.plot(x,Ecc,'b.')
-plt.hlines(bin_means,bin_edges[:-1],bin_edges[1:],colors='g',lw=5)
+bin_meansEcc, bin_edgesEcc, binnumberEcc = stats.binned_statistic(x,Ecc,statistic='mean',bins=10)
+
+bin_meansS, bin_edgesS, binnumberS = stats.binned_statistic(x,sharpness,statistic='mean',bins=10)
+
+
+
+plt.figure(1)
+plt.plot(x,R,'b.')
+plt.hlines(bin_meansR,bin_edgesR[:-1],bin_edgesR[1:],colors='g',lw=5)
 plt.title("Ampl vs distance from center")
+plt.xlabel("Distance from center")
+plt.ylabel("Amplitude")
+
+plt.figure(2)
+plt.plot(x,Ecc,'b.')
+plt.hlines(bin_meansEcc,bin_edgesEcc[:-1],bin_edgesEcc[1:],colors='g',lw=5)
+plt.title("Eccentricity vs distance from center")
+plt.xlabel("Distance from center")
+plt.ylabel("Eccentricity")
+
+
+plt.figure(3)
+plt.plot(x,sharpness,'b.')
+plt.hlines(bin_meansS,bin_edgesS[:-1],bin_edgesS[1:],colors='g',lw=5)
+plt.title("Sharpness vs distance from center")
+plt.xlabel("Distance from center")
+plt.ylabel("Sharpness")
+
 plt.show()
